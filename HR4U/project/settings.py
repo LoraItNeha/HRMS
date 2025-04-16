b498d7777af322f1b49b83f257277fe6ba2000f7
LoraItNeha <121551659+LoraItNeha@users.noreply.github.com>
2025-04-16 11:50:17 +0530

    Update settings.py

--- HR4U/project/settings.py
+++ HR4U/project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^vb_x5q7pck_rk*lwby^s2--&y902lpa71=11ix2miyeut+$)'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -147,8 +147,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'loraemployee@gmail.com'
-EMAIL_HOST_PASSWORD = 'yizz onvt vvgc hrei'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
